26b28858a1c44759266f854a2edace5c42fa67a2
tobitti0 <mail+git@tobitti.net>
2022-11-15 23:46:53 +0900

    Enable execution without LOGO analysis data

--- src/command/join_logo_frame.js
+++ src/command/join_logo_frame.js
@@ -1,5 +1,6 @@
 const childProcess = require('child_process');
 const path = require("path");
+const fs = require("fs-extra");
 
 const {
   JL_DIR,
@@ -12,8 +13,11 @@ const {
 
 exports.exec = param => {
   return new Promise((resolve)=>{
-    let args = [ "-inlogo",
-      LOGOFRAME_TXT_OUTPUT,
+    let args = [];
+    if( fs.existsSync( LOGOFRAME_TXT_OUTPUT ) ){
+      args.push( "-inlogo", LOGOFRAME_TXT_OUTPUT );
+    }
+    args.push(
       "-inscp",
       CHAPTEREXE_OUTPUT,
       "-incmd",
@@ -24,7 +28,7 @@ exports.exec = param => {
       JLSCP_OUTPUT,
       "-flags",
       param.JL_FLAGS
-    ];
+    );
 
     if (param.JLOGO_OPT1 && param.JLOGO_OPT1 !== "") {
       args = args.concat(param.JLOGO_OPT1.split(" "));
@@ -34,12 +38,13 @@ exports.exec = param => {
       args = args.concat(param.JLOGO_OPT2.split(" "));
     }
 
+    console.log(args)
     const child = childProcess.spawn(JLSCP_COMMAND, args);
     child.on('exit', (code)=>{
       resolve();
     });
     child.stderr.on('data', (data)=>{
-      console.error("join_logo_frame " + data);
+      console.error("join_logo_frame " + String(data).replace(/\n$/,""));
     });
   })
 };
